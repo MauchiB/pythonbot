e0617e947187bd9f10b3cc773550b63188658bfe
MauchiB <166924790+MauchiB@users.noreply.github.com>
2024-10-30 00:01:14 +0300

    Two commit
    delete file

--- config.py
+++
@@ -1,7 +0,0 @@
-TOKEN = '7865152331:AAESFCZn94aLbKxdgXbEqbY8mXukLercpNs'
-port = '5432'
-username = 'postgres'
-password = '372548sqlpython'
-database = 'pythonbot'
-host = '127.0.0.1'
-code = '123456789abcd'
